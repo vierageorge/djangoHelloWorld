ca7e139700141c9b95e9b212222e0a1264846b72
Jorge Andres Viera Olivero <Jorge.Viera@TigoUne.com>
2018-04-17 17:56:57 -0500

    Added more work on views and templates.
    Each action has a template.

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 #from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+from .models import Question, Choice
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -14,8 +15,21 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/results.html', context)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        context = {'question':question,'error_message':'Please pick a choice.'}
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
